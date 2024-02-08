61958888bf4b741b6e14a616392154aa59b0214b
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-02-08 12:12:27 +0300

    seed roles, admin

---
+++ EasyBlog/Data/ApplicationDbContextSeed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyBlog.Data
+{
+    public static class ApplicationDbContextSeed
+    {
+        public static async Task SeedAsync(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            if (!await roleManager.RoleExistsAsync("Administrator"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Administrator"));
+            }
+
+            if (!await userManager.Users.AnyAsync(u => u.UserName == "admin@example.com"))
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = "admin@example.com",
+                    Email = "admin@example.com",
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, "P@ssword1");
+                await userManager.AddToRoleAsync(user, "Administrator");
+            }
+        }
+    }
+}
