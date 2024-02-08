23df650a3fb327f1f97a15a4c88306e06e45c3c9
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2024-02-08 12:39:15 +0300

    Admin Area

---
+++ EasyBlog/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlog.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    [Area("Admin")]
+    public abstract class AdminBaseController : Controller
+    {
+    }
+}
---
+++ EasyBlog/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlog.Areas.Admin.Controllers
+{
+    public class DashboardController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
